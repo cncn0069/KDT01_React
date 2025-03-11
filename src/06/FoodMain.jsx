78bcda250f163880f2a4052ea5de19645127c23c
cncn0069 <cncn0069@naver.com>
2025-03-11 09:16:59 +0900

    1

--- src/06/FoodMain.jsx
+++ src/06/FoodMain.jsx
@@ -5,33 +5,70 @@ import TailButton from '../ui/TailButton';
 
 
 export default function FoodMain() {
-  //tags 생성
-  const [tags, setTags] = useState([]);
-  //setTags(FoodData.map(item => <FoodCard res={item} />));
 
-  let category = FoodData.map(item => item["운영주체 분류"].replaceAll(' ', ''));
+  let contents = FoodData.map(item => <FoodCard res={item} />);
 
-  //중복제거
-  category = [...new Set(category)];
 
-  const buttons = category.map(item => <TailButton key={item} caption={item} color={'bg-blue-400'} onClick={() => { handleOneClick(item) }} />)
-
-  const handleOneClick = (value) => {
-    let foodcard = FoodData.filter(item => (item["운영주체 분류"].replaceAll(' ', '') == value));
-    setTags(foodcard.map(item => <FoodCard res={item} />));
-
-  }
 
   return (
     //여기서 h를 풀로 안 해줘서 플렉스 박스가 짤렸음
     //TailButton({caption,color,onClick}
     <>
-        <div className='flex flex-row justify-center p-1'>
-          {buttons}
-        </div>
-        <div className='grid grid-cols-2 w-full gap-2 h-full overflow-y-auto'>
-        {tags}
-        </div>
+      <div className='grid grid-cols-2 overflow-y-auto gap-2'>
+        {contents}
+      </div>
+
     </>
   )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export default function FoodMain() {
+//   //tags 생성
+//   const [tags, setTags] = useState([]);
+//   //setTags(FoodData.map(item => <FoodCard res={item} />));
+
+//   let category = FoodData.map(item => item["운영주체 분류"].replaceAll(' ', ''));
+
+//   //중복제거
+//   category = [...new Set(category)];
+
+//   const buttons = category.map(item => <TailButton key={item} caption={item} color={'bg-blue-400'} onClick={() => { handleOneClick(item) }} />)
+
+//   const handleOneClick = (value) => {
+//     let foodcard = FoodData.filter(item => (item["운영주체 분류"].replaceAll(' ', '') == value));
+//     setTags(foodcard.map(item => <FoodCard res={item} />));
+
+//   }
+
+//   return (
+//     //여기서 h를 풀로 안 해줘서 플렉스 박스가 짤렸음
+//     //TailButton({caption,color,onClick}
+//     <>
+//         <div className='flex flex-row justify-center p-1'>
+//           {buttons}
+//         </div>
+//         <div className='grid grid-cols-2 w-full gap-2 h-full overflow-y-auto'>
+//         {tags}
+//         </div>
+//     </>
+//   )
+// }
